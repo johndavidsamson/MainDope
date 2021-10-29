b4454e47c8b7689a61990b51889b0c7aa3985896
johndavidsamson <samsonjdb@gmail.com>
2021-10-29 12:40:07 +0800

    Add files via upload

---
+++ Main Dope pt.2/MainDope2/src/maindope2/floorDialogue.java
@@ -0,0 +1,71 @@
+package maindope2;
+
+public class floorDialogue {
+    
+    Player player;
+    
+    public static void secondFloorDialogue(Player player){
+        System.out.println( player.name + " : Wow! After all the forces I have encountered during the second area here I am\n"
+                + "in this dark place so scary!\n"
+                + "Who is this person standing in this massive door that is encapsulated with evil forces?\n"
+                + player.name + " : Hey! It's one of the villagers missing people I should go talk to her.\n"
+                + "Miss Kobayashi: Why are you here?! It is very dangerous!\n"
+                + player.name + " : I am here to defeat the boss pf this dungeon!\n"
+                + "Miss Kobayashi: It is impossible! He is far more stronger than you!\n"
+                + player.name + " : I don't even care a bit ha! And results lie at the end.\n"
+                + "Miss Kobayashi: So you do have the courage to fight the strongest boss so far?\n"
+                + player.name + " : Of course to retrieve the Gem is my mission after all.\n"
+                + "Miss Kobayashi: Well he is the strongest of themm all.\n"
+                + player.name + " : Ha! seems his weak after all no skills needed\n"
+                +  player.name + " : I'll defeat him don't be scared. It's safe now go home already.\n"
+                + "Miss Kobayashi : Thank you very much," + player.name + " for saving me!"
+                + "[Press Enter to enter to start the battle]");
+    }
+    
+    public static void thirdFloorDialogue(Player player){
+        System.out.println(player.name + " : The dark forces seem to never stop!\n"
+                + " Time to continue this journey.\n"
+                + " Good thing there is a guard here I could talk to her for a little while"
+                + " she seems to be a good guard.\n"
+                + "Chibisuke: Good day, my name is Chibisuke, and you are?\n"
+                + " My name is " + player.name + " glad to meet you.\n"
+                + "Chibisuke: What are you here for?\n"
+                + player.name + " : I am here to cleanse this area and free the evil spirits.\n"
+                + "Chibisuke: Okay! I will allow you to enter this dungeon but be carful this area will be much more challenging\n  "
+                + player.name + " : Thank you for allowing me. It's a great chat with you , but you are so kawaii.\n"
+                + " [Press Enter to enter to start the battle]");
+    }
+    
+    public static void fourthFloorDialogue(Player player){
+        System.out.println( player.name + " : Who is this beautiful girl sitting inside this prison\n"
+                + "Caramel : Please free me!\n"
+                +  player.name + " : Oh! I recognize you! You are also from the village!\n"
+                + "Caramel : Yes, I am one of the beautiful girls you would encounter when yoy go to our village\n"
+                + player.name + " : Okay? I'm going to free you, but how many are you in this fucking dungeon?!\n"
+                + "");
+    }
+    
+    public static void fifthFloorDialogue(){
+        
+    }
+    
+    public static void sixthFloorDialogue(){
+        
+    }
+    
+    public static void seventhFloorDialogue(){
+        
+    }
+    
+    public static void eighthFloorDialogue(){
+        
+    }
+    
+    public static void ninthFloorDialogue(){
+        
+    }
+    
+    public static void tenthFloorDialogue(){
+        
+    }
+}
